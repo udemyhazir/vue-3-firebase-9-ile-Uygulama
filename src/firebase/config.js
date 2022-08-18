3a9161ab7ddb13f27267abde6f9d5f4a5f44f9d4
AOS UDEMY <97443703+udemyhazir@users.noreply.github.com>
2022-08-18 09:30:20 +0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,12 +6,7 @@ import {getFirestore} from 'firebase/firestore'
 
 const firebaseConfig = {
 
-    apiKey: "AIzaSyBPR1U8phPrGgUXfytfp0j2xnln-KIcqbk",
-    authDomain: "aos-sosyal-app.firebaseapp.com",
-    projectId: "aos-sosyal-app",
-    storageBucket: "aos-sosyal-app.appspot.com",
-    messagingSenderId: "416249935872",
-    appId: "1:416249935872:web:f8995db40cef4c360480e5"
+    ....
 };
 
 
